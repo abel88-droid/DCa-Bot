78b2cb3dcfd93a9b1fa7a15f7de6b946c44c2e8a
abel88-droid <83132070+abel88-droid@users.noreply.github.com>
2025-02-27 20:00:11 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "dca-bot",
+  "version": "1.0.0",
+  "description": "A Discord bot built using discord.js",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "discord.js": "^14.11.0",
+    "pg": "^8.11.1",
+    "dotenv": "^16.0.3"
+  }
+}
