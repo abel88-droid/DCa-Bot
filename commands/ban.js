4682c28a59951371c91a12a9ff7fb84469009b7f
abel88-droid <83132070+abel88-droid@users.noreply.github.com>
2025-02-28 18:52:33 +0530

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,30 +1,22 @@
-const { PermissionsBitField } = require("discord.js");
-
 module.exports = {
-    name: "ban",
-    description: "Bans a user from the server",
-    async execute(message, args) {
-        if (!message.member.permissions.has(PermissionsBitField.Flags.BanMembers)) {
-            return message.reply("You do not have permission to ban members.");
-        }
-
-        const user = message.mentions.members.first();
-        if (!user) {
-            return message.reply("Please mention a user to ban.");
-        }
-
-        const reason = args.slice(1).join(" ") || "No reason provided";
-
-        if (!message.guild.members.me.permissions.has(PermissionsBitField.Flags.BanMembers)) {
-            return message.reply("I do not have permission to ban members.");
-        }
-
-        try {
-            await user.ban({ reason });
-            message.channel.send(`**${user.user.tag**} has been banned. **Reason**: *${reason}*`);
-        } catch (error) {
-            console.error(error);
-            message.reply("I was unable to ban the member.");
-        }
+  name: "ban",
+  async execute(message, args) {
+    if (!message.member.permissions.has("BAN_MEMBERS")) {
+      return message.reply("You don't have permission to use this command.");
     }
+
+    const target = message.mentions.members.first();
+    const reason = args.slice(1).join(" ") || "No reason provided";
+
+    if (!target) {
+      return message.reply("Please mention a user to ban.");
+    }
+
+    if (!target.bannable) {
+      return message.reply("I cannot ban this user.");
+    }
+
+    await target.ban({ reason });
+    message.channel.send(`**${target.user.tag}** has been banned. Reason: **${reason}**`);
+  },
 };
