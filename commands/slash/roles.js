93730b9e23138eb8b070a3315560d723f7d3e908
abel88-droid <83132070+abel88-droid@users.noreply.github.com>
2025-03-02 00:01:08 +0530

    Update roles.js

--- commands/slash/roles.js
+++ commands/slash/roles.js
@@ -3,48 +3,21 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("roles")
-        .setDescription("Manage user roles")
+        .setDescription("Manage roles")
         .addSubcommand(subcommand =>
-            subcommand.setName("add")
+            subcommand
+                .setName("add")
                 .setDescription("Add a role to a user")
                 .addUserOption(option =>
                     option.setName("user")
-                        .setDescription("The user to add the role to")
+                        .setDescription("User to assign the role")
                         .setRequired(true))
                 .addRoleOption(option =>
                     option.setName("role")
-                        .setDescription("The role to add")
-                        .setRequired(true))
-        )
-        .addSubcommand(subcommand =>
-            subcommand.setName("remove")
-                .setDescription("Remove a role from a user")
-                .addUserOption(option =>
-                    option.setName("user")
-                        .setDescription("The user to remove the role from")
-                        .setRequired(true))
-                .addRoleOption(option =>
-                    option.setName("role")
-                        .setDescription("The role to remove")
+                        .setDescription("Role to assign")
                         .setRequired(true))
         ),
-
     async execute(interaction) {
-        const subcommand = interaction.options.getSubcommand();
-        const user = interaction.options.getUser("user");
-        const role = interaction.options.getRole("role");
-        const member = interaction.guild.members.cache.get(user.id);
-
-        if (!interaction.member.permissions.has("MANAGE_ROLES")) {
-            return interaction.reply({ content: "❌ You don’t have permission to manage roles!", ephemeral: true });
-        }
-
-        if (subcommand === "add") {
-            await member.roles.add(role);
-            return interaction.reply({ content: `✅ Successfully added ${role} to ${user}.` });
-        } else if (subcommand === "remove") {
-            await member.roles.remove(role);
-            return interaction.reply({ content: `✅ Successfully removed ${role} from ${user}.` });
-        }
-    }
+        await interaction.reply("Roles command executed!");
+    },
 };
