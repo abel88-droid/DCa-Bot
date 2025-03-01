6ff194e93e1d531233f2e44d77b941a261b3417f
abel88-droid <83132070+abel88-droid@users.noreply.github.com>
2025-03-01 20:05:35 +0530

    Update ban.js

--- commands/text/ban.js
+++ commands/text/ban.js
@@ -1,1 +1,22 @@
+module.exports = {
+  name: "ban",
+  async execute(message, args) {
+    if (!message.member.permissions.has("BAN_MEMBERS")) {
+      return message.reply("You don't have permission to use this command.");
+    }
 
+    const target = message.mentions.members.first();
+    const reason = args.slice(1).join(" ") || "No reason provided";
+
+    if (!target) {
+      return message.reply("Please mention a user to ban.");
+    }
+
+    if (!target.bannable) {
+      return message.reply("I cannot ban this user.");
+    }
+
+    await target.ban({ reason });
+    message.channel.send(`**${target.user.tag}** has been banned. Reason: **${reason}**`);
+  },
+};
