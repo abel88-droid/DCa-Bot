f8bb967d8d6d1c4fa06a04023c6eb7ff7e0ccb85
abel88-droid <83132070+abel88-droid@users.noreply.github.com>
2025-03-03 17:37:48 +0530

    Update reactionRoles_unlockchannel2.js

--- events/reactionRoles_unlockchannel2.js
+++ events/reactionRoles_unlockchannel2.js
@@ -1,42 +1,41 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Events } = require("discord.js");
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMembers] });
+module.exports = {
+    name: Events.MessageReactionAdd, 
+    async execute(reaction, user) {
+        if (reaction.message.id !== "1346086839299080218" || user.bot) return;
 
-const TOKEN = "MTM0MDIyMjk3MTg0NzExNDc2Mg.Gus_c1.rcewlIIP4EDlT3PMmYEgSMCOo-NcBqp7Vdt2B4";
-const CHANNEL_ID = "1239880291523366942";
-const MESSAGE_ID = "1346086839299080218";
+        const roles = {
+            "1️⃣": "1346087307538599956",
+            "2️⃣": "1346088650999464006",
+            "3️⃣": "1346088973004574851",
+            "4️⃣": "1346089335883042880",
+        };
 
-const roles = {
-    "1️⃣": "1346087307538599956",
-    "2️⃣": "1346088650999464006",
-    "3️⃣": "1346088973004574851",
-    "4️⃣": "1346089335883042880",
+        const roleId = roles[reaction.emoji.name];
+        if (!roleId) return;
+
+        const member = await reaction.message.guild.members.fetch(user.id);
+        await member.roles.add(roleId);
+    },
 };
 
-client.once("ready", () => {
-    console.log(`Logged in as ${client.user.tag}`);
-});
+module.exports = {
+    name: Events.MessageReactionRemove, 
+    async execute(reaction, user) {
+        if (reaction.message.id !== "1346086839299080218" || user.bot) return;
 
-client.on("messageReactionAdd", async (reaction, user) => {
-    if (reaction.message.id !== MESSAGE_ID || user.bot) return;
-    
-    const roleId = roles[reaction.emoji.name];
-    if (!roleId) return;
+        const roles = {
+            "1️⃣": "1346087307538599956",
+            "2️⃣": "1346088650999464006",
+            "3️⃣": "1346088973004574851",
+            "4️⃣": "1346089335883042880",
+        };
 
-    const guild = reaction.message.guild;
-    const member = await guild.members.fetch(user.id);
-    await member.roles.add(roleId);
-});
+        const roleId = roles[reaction.emoji.name];
+        if (!roleId) return;
 
-client.on("messageReactionRemove", async (reaction, user) => {
-    if (reaction.message.id !== MESSAGE_ID || user.bot) return;
-
-    const roleId = roles[reaction.emoji.name];
-    if (!roleId) return;
-
-    const guild = reaction.message.guild;
-    const member = await guild.members.fetch(user.id);
-    await member.roles.remove(roleId);
-});
-
-client.login(TOKEN);
+        const member = await reaction.message.guild.members.fetch(user.id);
+        await member.roles.remove(roleId);
+    },
+};
