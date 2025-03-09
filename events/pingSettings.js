ebd88fc75209724a4d9619b11d7a9ab22f55c30d
abel88-droid <83132070+abel88-droid@users.noreply.github.com>
2025-03-09 11:44:49 +0530

    Create pingSettings.js

---
+++ events/pingSettings.js
@@ -0,0 +1,37 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const channelId = "1239880291523366942"; 
+const firstLine = "It's not necessary to join every time it's reset."; 
+const fullMessage = 
+  `${firstLine}\nIf you are in doubt about what you will get pinged for, check your roles. You should have some of these:`;
+
+module.exports = {
+  name: "ready",
+  once: true,
+  async execute(client) {
+    const channel = client.channels.cache.get(channelId);
+    if (!channel) return console.log("Channel not found!");
+
+    try {
+      // Fetch last 10 messages in the channel
+      const messages = await channel.messages.fetch({ limit: 10 });
+
+      // Check if any message starts with the first line of our message
+      const existingMessage = messages.find(m => m.content.startsWith(firstLine));
+
+      if (existingMessage) {
+        console.log("Message already exists. Skipping.");
+        return; 
+      }
+
+      
+      await channel.send({
+        content: fullMessage,
+        files: ["https://cdn.discordapp.com/attachments/839907517663936612/1103713190593953872/20230504_180004.jpg?ex=67ce6442&is=67cd12c2&hm=52763e61604eb3f69b49a3eaca2e96015c17360c523441d3e67c5a22abd26c54&"], 
+      });
+
+    } catch (error) {
+      console.error("Error sending message:", error);
+    }
+  }
+};
