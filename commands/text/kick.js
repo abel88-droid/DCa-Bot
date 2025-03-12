02c9e02443d83b1100cd67540eb0d930dc2e9ba9
abel88-droid <83132070+abel88-droid@users.noreply.github.com>
2025-03-12 19:58:16 +0530

    Update kick.js

--- commands/text/kick.js
+++ commands/text/kick.js
@@ -1,11 +1,9 @@
-const { PermissionsBitField } = require("discord.js");
+const { PermissionsBitField, EmbedBuilder } = require("discord.js");
 
 module.exports = {
   name: "kick",
   description: "Kicks a user from the server",
   execute(message, args) {
-    if (!message.content.startsWith("-kick")) return; // Ensures it only triggers on "-kick"
-
     if (!message.member.permissions.has(PermissionsBitField.Flags.KickMembers)) {
       return message.reply("You do not have permission to use this command.");
     }
@@ -20,13 +18,30 @@ module.exports = {
     }
 
     if (args.length < 2) {
-      return message.reply("You must provide a reason for the kick. Usage: `-kick @user [reason]`");
+      return message.reply("Please provide a reason for the kick.");
     }
 
     const reason = args.slice(1).join(" ");
+    const action = "Kicked";
 
+    // DM Embed
+    const dmEmbed = new EmbedBuilder()
+      .setColor("RED")
+      .setTitle(`You have been ${action}`)
+      .setDescription(`You have been **${action}** from **${message.guild.name}**.`)
+      .addFields({ name: "Reason", value: reason })
+      .setFooter({ text: "If you think this is a mistake, please contact |DC|SockS#0724, dc_devil. or Gorilla Kurt#2758." })
+      .setTimestamp();
+
+    member.send({ embeds: [dmEmbed] }).catch(() => {
+      console.log(`Could not send DM to ${member.user.tag}`);
+    });
+
+    // Kick the user
     member.kick(reason)
-      .then(() => message.reply(`Successfully kicked **${member.user.tag}**. Reason: ${reason}`))
+      .then(() => {
+        message.reply(`Successfully kicked **${member.user.tag}**. Reason: ${reason}`);
+      })
       .catch(error => {
         console.error(error);
         message.reply("An error occurred while trying to kick the user.");
