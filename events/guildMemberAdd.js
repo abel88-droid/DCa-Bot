d1b90cfb727232f325a35225611de40d38542fa3
Ash_in <83132070+abel88-droid@users.noreply.github.com>
2025-05-20 13:44:52 +0530

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: "guildMemberAdd",
     execute(member) {
-        const channel = member.guild.channels.cache.get("1239879910118654016"); // Update with your channel ID
+        const channel = member.guild.channels.cache.get("916042813425201152"); // Update with your channel ID
         if (!channel) return;
 
-        channel.send(` **Welcome** ${member}, 👋\n\nIf you **haven't** read the <#1239880290026000385>, please do.\nThere is an explanation of the different roles in here too. Read them here <#1340644055855399005>.\n\nThere are more channels than you can see. Go to <#1239880291523366942> and select/deselect them.\n\nIf you want access to Discord Driver main other channel, please contact your leaders.\n\n**Hope you will have fun in DC driver Alliance** 👍`);
+        channel.send(` **Welcome** ${member}, 👋\n\nIf you **haven't** read the <#839605609027600415>, please do.\nThere is an explanation of the different roles in here too. Read them here <#981257687008428082>.\n\nThere are more channels than you can see. Go to <#840310137390104627> and select/deselect them.\n\nIf you want access to Discord Driver main other channel, please contact your leaders.\n\n**Hope you will have fun in DC driver Alliance** 👍`);
     }
 };
