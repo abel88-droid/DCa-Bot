2014d13ff95d4836365eb05546157c687e244566
Ash_in <83132070+abel88-droid@users.noreply.github.com>
2025-05-21 19:04:35 +0530

    Update ban.js

--- commands/text/ban.js
+++ commands/text/ban.js
@@ -5,42 +5,34 @@ module.exports = {
       return message.reply("You don't have permission to use this command.");
     }
 
-    const targetId = args[0]; // First argument could be a mention or an ID
-    const reason = args.slice(1).join(" "); // Now reason is required
+    const targetId = args[0]; // First argument should be a user ID
+    const reason = args.slice(1).join(" ");
 
     if (!targetId) {
-      return message.reply("Please provide a user to ban.");
+      return message.reply("Please provide a user ID to ban.");
     }
 
     if (!reason) {
       return message.reply("Please provide a reason for the ban.");
     }
 
-    let target = message.mentions.members.first();
-
-    // If no mention is found, try fetching by ID
-    if (!target) {
-      try {
-        target = await message.guild.members.fetch(targetId);
-      } catch {
-        return message.reply("User not found.");
-      }
-    }
-
-    if (!target.bannable) {
-      return message.reply("I cannot ban this user.");
-    }
-
-    // DM the user before banning them
+    // DM the user before banning (only works if they haven't blocked the bot)
     try {
-      await target.send(
+      const user = await message.client.users.fetch(targetId);
+      await user.send(
         `You have been banned from **${message.guild.name}**.\nReason: **${reason}**\n\nIf you think this is a mistake, please contact |DC|SockS#0724, dc_devilx or gorillakurt.`
       );
     } catch {
       message.channel.send("Couldn't send a DM to the user. Proceeding with the ban.");
     }
 
-    await target.ban({ reason });
-    message.channel.send(`**${target.user.tag}** has been banned. Reason: **${reason}**`);
+    // Ban the user by ID
+    try {
+      await message.guild.bans.create(targetId, { reason });
+      message.channel.send(`**User with ID ${targetId}** has been banned. Reason: **${reason}**`);
+    } catch (error) {
+      console.error(error);
+      message.reply("Failed to ban the user. They may not exist or already be banned.");
+    }
   },
 };
