4e0469500aa253818990a64b042cf9aef0ffbc0a
Ash_in <83132070+abel88-droid@users.noreply.github.com>
2025-06-18 17:46:13 +0530

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -4,6 +4,18 @@ module.exports = {
         const channel = member.guild.channels.cache.get("916042813425201152"); // Update with your channel ID
         if (!channel) return;
 
-        channel.send(` **Welcome** ${member}, 👋\n\nIf you **haven't** read the <#839605609027600415>, please do.\nThere is an explanation of the different roles in here too. Read them here <#981257687008428082>.\n\nThere are more channels than you can see. Go to <#840310137390104627> and select/deselect them.\n\nIf you want access to Discord Driver main other channel, please contact your leaders.\n\n**Hope you will have fun in DC driver Alliance** 👍`);
+        channel.send(`Hey @${member.user.username}! 👋
+Welcome to the **Discord Alliance server**!
+
+**Getting Started**:
+> 1. Please go to <#839605609027600415> and read the server rules. You’ll get access to the main channels after you press ☑️.
+
+> 2. There are many more channels available. Head over to <#840310137390104627> and select your options to unlock them.
+
+> 3. Please make sure your in-game name and Discord display name are the same on this server, so your leaders can identify you easily (it's mandatory).
+
+If you want access to more channels in the **Discord Drivers server**, please contact your team leader.
+
+**Have fun and enjoy your time here!**`);
     }
 };
